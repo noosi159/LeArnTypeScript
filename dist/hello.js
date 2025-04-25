0df454762c3854216d8defe419165ab5a35e3c98
noosi <noosi4567@gmail.com>
2025-04-25 14:46:31 +0700

    Day 1

---
+++ dist/hello.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello World");
+let j = 10;
+// j = "JavaScript" // จะเกิดปัญหาเพราะ j ถูกประกาศเป็นชนิด number
+let k = Math.pow(j, 2);
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ hello.ts
@@ -0,0 +1,10 @@
+console.log("Hello World")
+
+let j = 10
+// j = "JavaScript" // จะเกิดปัญหาเพราะ j ถูกประกาศเป็นชนิด number
+
+let k = Math.pow(j, 2)
+
+const sum = (a: number, b: number): number => {
+  return a + b
+}
